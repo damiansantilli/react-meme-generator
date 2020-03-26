b320924517853bb73b5e593b26367102d9d1e4f0
Damian Santilli <62546804+damiansantilli@users.noreply.github.com>
2020-03-26 15:38:47 -0400

    Add files via upload

--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,13 @@ import React from "react"
 
 function Header() {
     return (
-         <div class="section">
-            <div class="w-container">
+        <div class="section">
+           <div class="w-container">
                 <div class="w-row">
-                    <div class="w-col w-col-10">
+                    <div class="w-col w-col-10 w-col-small-10 w-col-tiny-10">
                         <h1 class="heading titulo-header">React Meme Generator</h1>
                     </div>
-                    <div class="w-col w-col-2"><img src="trollface.png" alt="" class="image"/></div>
+                    <div class="column w-col w-col-2 w-col-small-2 w-col-tiny-2"><img src="Trollface.png" alt="" class="image"/></div>
                 </div>
             </div>
         </div>
--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -37,7 +37,7 @@ class MemeGenerator extends Component {
     render() {
         return (
             <div>
-                <div class="w-container">
+                {/* <div class="w-container">
                     <div class="w-form">
                         <form id="email-form" name="email-form" data-name="Email Form" class="form" onSubmit={this.handleSubmit}>
                             <div class="w-row">
@@ -73,8 +73,51 @@ class MemeGenerator extends Component {
                     <div class="w-container">
                         <p class="paragraph"><a href="http://damian.epizy.com/" target="_blank"><span class="text-span">Damian Santilli</span></a></p>
                     </div>
-                </div>
+                </div> */}
 
+                {/* AQUI */}
+                
+                <div class="w-container">
+                    <div class="form-block w-form">
+                    <form id="email-form" name="email-form" data-name="Email Form" class="form" onSubmit={this.handleSubmit}>
+                        <div class="w-row">
+                            <div class="w-col w-col-5 w-col-small-5 w-col-tiny-4"><label for="name">Text Top</label>
+
+                                <input 
+                                    type="text"
+                                    name="topText"
+                                    value={this.state.topText}
+                                    onChange={this.handleChange}
+                                    class="w-input"
+                                /> 
+                            
+                            </div>
+                            <div class="w-col w-col-5 w-col-small-5 w-col-tiny-4"><label for="email">Bottom Text</label>
+
+                                <input 
+                                    type="text"
+                                    name="bottomText"
+                                    value={this.state.bottomText}
+                                    onChange={this.handleChange}
+                                    class="w-input"
+                                />
+                                
+                            </div>
+                            <div class="w-col w-col-2 w-col-small-2 w-col-tiny-4"><input type="submit" value="GEN" data-wait="Please wait..." class="submit-button w-button"/></div>
+                        </div>
+                    </form>
+                    </div>
+                    <div class="box-img-meme-title">
+                        <img src={this.state.randomImg} alt="" class="img-meme"/>
+                        <h2 class="heading-2 text-top">{this.state.topText}</h2>
+                        <h2 class="heading-3 bottom-text">{this.state.bottomText}</h2>
+                    </div>
+                </div>
+                <div class="section-2">
+                    <div class="w-container">
+                        <p class="paragraph"><a href="http://damian.epizy.com/" target="_blank"><span class="text-span">Damian Santilli</span></a></p>
+                    </div>
+                </div>
 
 
 
