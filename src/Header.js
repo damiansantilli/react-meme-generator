095207fe188b7482946a7a311e7ce905473fb5d7
Damian Santilli <62546804+damiansantilli@users.noreply.github.com>
2020-03-26 17:39:24 -0400

    Add files via upload

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ function Header() {
                     <div class="w-col w-col-10 w-col-small-10 w-col-tiny-10">
                         <h1 class="heading titulo-header">React Meme Generator</h1>
                     </div>
-                    <div class="column w-col w-col-2 w-col-small-2 w-col-tiny-2"><img src="Trollface.png" alt="" class="image"/></div>
+                    <div class="column w-col w-col-2 w-col-small-2 w-col-tiny-2"><img src="trollface.png" alt="" class="image"/></div>
                 </div>
             </div>
         </div>
